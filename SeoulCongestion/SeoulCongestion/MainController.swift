8404cbbcb493f5757ce17fc8a4557c2e9d1657ba
장일규 <jang19.dev@gmail.com>
2023-01-25 21:03:32 +0900

    Add: Initial Project

---
+++ SeoulCongestion/SeoulCongestion/MainController.swift
@@ -0,0 +1,16 @@
+//
+//  MainController.swift
+//  SeoulCongestion
+//
+//  Created by jangilkyu on 2023/01/25.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+}
