f3c35c08ff61f0c92697ad036b28dd10319f6d5f
장일규 <jang19.dev@gmail.com>
2023-10-29 02:53:17 +0900

    Update: Change to Application folder level

--- SeoulCongestion/SeoulCongestion/Application/SceneDelegate.swift
+++ SeoulCongestion/SeoulCongestion/Application/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(
         _ scene: UIScene, willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
@@ -23,30 +22,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.overrideUserInterfaceStyle = .light
         self.window = window
         window.makeKeyAndVisible()
-    }
-
-    func sceneDidDisconnect(
-        _ scene: UIScene
-    ) {
-    }
-
-    func sceneDidBecomeActive(
-        _ scene: UIScene
-    ) {
-    }
-
-    func sceneWillResignActive(
-        _ scene: UIScene
-    ) {
-    }
-
-    func sceneWillEnterForeground(
-        _ scene: UIScene
-    ) {
-    }
-
-    func sceneDidEnterBackground(
-        _ scene: UIScene
-    ) {
     }
 }
